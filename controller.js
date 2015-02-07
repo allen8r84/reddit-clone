adbdd443f0e9463ca1e890c095de33f2742a6199
Jon <jon@allen8r.com>
2015-02-07 12:27:54 -0700

    create form for reddit

--- controller.js
+++ controller.js
@@ -2,4 +2,22 @@ var app = angular.module('reddit');
 
 app.controller('PostController', function($scope){
     $scope.test = "Welcome to reddit";
+    $scope.posts = [];
+    $scope.addPost = function() {
+        $scope.newPost.timestamp =  Date.now();
+        $scope.newPost.karma = 0;
+        $scope.newPost.comment = [];
+        console.log($scope.newPost);
+        
+        $scope.posts.push($scope.newPost);
+        $scope.reset(); //always at the end
+    }
+
+    $scope.reset = function() {
+        $scope.newPost = {};
+    }
+
+
+
+
 });
