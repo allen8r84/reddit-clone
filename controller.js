a033151cc7f68d884654cfd60eb63f207bb74d83
Jon <jon@allen8r.com>
2015-02-07 13:43:20 -0700

    finish class stuff

--- controller.js
+++ controller.js
@@ -6,7 +6,7 @@ app.controller('PostController', function($scope){
     $scope.addPost = function() {
         $scope.newPost.timestamp =  Date.now();
         $scope.newPost.karma = 0;
-        $scope.newPost.comment = [];
+        $scope.newPost.comments = [];
         console.log($scope.newPost);
         
         $scope.posts.push($scope.newPost);
@@ -16,7 +16,18 @@ app.controller('PostController', function($scope){
     $scope.reset = function() {
         $scope.newPost = {};
     }
-
+    
+    $scope.vote = function(index, direction) {
+        if (direction === 'up') {
+            $scope.posts[index].karma++;
+        } else if (direction === 'down') {
+            $scope.posts[index].karma--;
+        }
+    }
+    $scope.submitComment = function(index, comment) {
+        $scope.posts[index].comments.push(comment);
+        $scope.posts[index].commentForm = '';
+    }
 
 
 
